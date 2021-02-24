a97edee35c4edda30deb0932a5bba1daa22a58ec
tonguwin <ttn135@ucsd.edu>
2021-02-23 20:26:32 -0800

    added import line to browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,9 @@ import React from 'react';
 import AppProvider from 'store/provider';
 import wrapPageElementWithTransition from 'helpers/wrapPageElement';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 // React Context in Browser
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({ element }) => {
